ddfde5ad09a303ff7a31bf661548be73a567c7b4
Minjoon Kim <minjoon.kim89@gmail.com>
2022-06-01 20:55:12 +0900

    spontaneously added interview question re-do

--- rt_notebook.py
+++ rt_notebook.py
@@ -4,6 +4,7 @@
 
 #%%
 # import all libraries
+from pickletools import long4
 import pandas as pd 
 import numpy as np 
 import seaborn as sns 
@@ -37,3 +38,39 @@ df_melted.head()
 # l/d = like, dislike, or dunno 
 # rt = float
 # watched = watched or not
+
+
+# %%
+# google interview question re-do
+data = {'Year': [2022, 2021, 2020, 2019],
+        'Jan': [20, 21, 19, 18], 
+        'Feb': [20, 21, 19, 18],
+        'Mar': [20, 21, 19, 18],
+        'Apr': [20, 21, 19, 18],
+        'May': [20, 21, 19, 18],
+        'Jun': [20, 21, 19, 18],
+        'Jul': [20, 21, 19, 18],
+        'Aug': [20, 21, 19, 18],
+        'Sep': [20, 21, 19, 18],
+        'Oct': [20, 21, 19, 18],
+        'Nov': [20, 21, 19, 18],
+        'Dec': [20, 21, 19, 18],
+        }
+
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+wide = pd.DataFrame(data)
+wide 
+# %%
+# long format check
+long = wide.melt(id_vars=['Year'], var_name='Month', value_name='Value')
+long
+# %%
+# sort by year and month?
+long['Month'] = pd.Categorical(long['Month'], categories=months, ordered=True)
+long.sort_values(['Year', 'Month'], ascending=[False, True], inplace=True)
+long.reset_index(inplace=True, drop=True)
+long
+
+# %%
