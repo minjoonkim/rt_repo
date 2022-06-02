2d389e54aac5da243a8fae530dba62facc890852
Minjoon Kim <minjoon.kim89@gmail.com>
2022-06-02 16:44:53 +0900

    done creating reusable original datasets

--- rt_notebook.py
+++ rt_notebook.py
@@ -4,7 +4,7 @@
 
 #%%
 # import all libraries
-from pickletools import long4
+# from pickletools import long4
 import pandas as pd 
 import numpy as np 
 import seaborn as sns 
@@ -28,49 +28,45 @@ bdf = bdf.drop('email', axis=1)
 bdf.head()
 # %%
 # make wide-format in to long-format
-df_melted = bdf.melt(id_vars=['wid', 'timestamp'])
-df_melted.sort_values(['wid', 'timestamp'], inplace=True)
-df_melted.head()
+bdf_melted = bdf.melt(id_vars=['wid', 'timestamp'])
+bdf_melted.sort_values(['wid', 'timestamp'], inplace=True)
+bdf_melted.reset_index(drop=True, inplace=True)
+bdf_melted.head()
 # %%
 # convert current data to 'analyzable' data... 
-# id(timestamp), movie, l/d, rt, watched
-# movie = does not include '_'
-# l/d = like, dislike, or dunno 
-# rt = float
-# watched = watched or not
+# new df = id(timestamp), movie, l/d, rt, watched
+# all data in 4 row pairs.
+# every 0: rating, 1: rt, 2: conf, 3: watched
 
+df_B = pd.DataFrame(columns=['id', 'title', 'rating', 'rt', 'conf', 'watched'])
+df_B['id'] = bdf_melted.iloc[::4]['timestamp']
+df_B['title'] = bdf_melted.iloc[::4]['variable']
+df_B['rating'] = bdf_melted.iloc[::4]['value']
+df_B.reset_index(drop=True, inplace=True)
 
+temp = bdf_melted.iloc[1::4]['value']
+temp.reset_index(drop=True, inplace=True)
+df_B['rt'] = temp 
+
+temp = bdf_melted.iloc[2::4]['value']
+temp.reset_index(drop=True, inplace=True)
+df_B['conf'] = temp 
+
+temp = bdf_melted.iloc[3::4]['value']
+temp.reset_index(drop=True, inplace=True)
+df_B['watched'] = temp 
 # %%
-# google interview question re-do
-data = {'Year': [2022, 2021, 2020, 2019],
-        'Jan': [20, 21, 19, 18], 
-        'Feb': [20, 21, 19, 18],
-        'Mar': [20, 21, 19, 18],
-        'Apr': [20, 21, 19, 18],
-        'May': [20, 21, 19, 18],
-        'Jun': [20, 21, 19, 18],
-        'Jul': [20, 21, 19, 18],
-        'Aug': [20, 21, 19, 18],
-        'Sep': [20, 21, 19, 18],
-        'Oct': [20, 21, 19, 18],
-        'Nov': [20, 21, 19, 18],
-        'Dec': [20, 21, 19, 18],
-        }
-
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-wide = pd.DataFrame(data)
-wide 
+# save original 'B' dataframe
+df_B.to_csv('desktop_rt_data.csv', index=False)
 # %%
-# long format check
-long = wide.melt(id_vars=['Year'], var_name='Month', value_name='Value')
-long
+# read in mobile data 
+df_A = pd.read_csv('data/db_0504.csv')
+df_A.head()
 # %%
-# sort by year and month?
-long['Month'] = pd.Categorical(long['Month'], categories=months, ordered=True)
-long.sort_values(['Year', 'Month'], ascending=[False, True], inplace=True)
-long.reset_index(inplace=True, drop=True)
-long
-
+df_A = df_A[['uID', 'title', 'rating', 'time']]
+df_A.rename({'uID': 'id', 'time': 'rt'}, axis=1, inplace=True)
+df_A.head()
+# %%
+# save original 'A' dataframe
+df_A.to_csv('mobile_rt_data.csv', index=False)
 # %%
